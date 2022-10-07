4d4236d85ecbc497ae601cf132bdf511bfae4e3e
Ali Arkan <alidark.sf@gmail.com>
2022-10-07 15:59:55 +0300

    content feunction has been added

--- src/main/java/base/BaseTests.java
+++ src/main/java/base/BaseTests.java
@@ -30,9 +30,15 @@ public class BaseTests {
         List<WebElement> links = driver.findElements(By.tagName("a"));
         System.out.println(links.size());
 
-        //Click Input link
-       WebElement inputsLinks= driver.findElement(By.linkText("Inputs"));
+        //getText by tagname
+        WebElement tag1 = driver.findElement(By.tagName("h2"));
+        System.out.println(tag1.getText());
+
+
+        WebElement inputsLinks= driver.findElement(By.linkText("Inputs"));
        inputsLinks.click();
+
+
 
 
 
@@ -44,16 +50,54 @@ public class BaseTests {
         driver.quit();
     }
 
+    public void shiftContent() throws  InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        // create a new driver
+        driver = new ChromeDriver();
+        //Open a internet site
+        driver.get("https://the-internet.herokuapp.com/");
+
+         /* 1. Click Shift Content
+        2. Click : Example 1: Menu Element
+        3. How many : home, about
+         */
+
+        //Click Shift Content
+        WebElement clickshiftbutton= driver.findElement(By.linkText("Shifting Content"));
+        clickshiftbutton.click();
+
+        //Click : Example 1: Menu Element
+        WebElement menuelement= driver.findElement(By.linkText("Example 1: Menu Element"));
+        menuelement.click();
+
+        List<WebElement> menuitems = driver.findElements(By.tagName("li"));
+        System.out.println("Menu size is: " + menuitems.size());
+        //System.out.println("Name of third item is: " + menuitems.get(2).getText());
+
+        for (int i=0;i<menuitems.size();i++)
+        {
+           var name= menuitems.get(i).getText();
+            System.out.println(name);
+        }
+
+        // Quit the browser
+        driver.quit();
+
+    }
+
 
     public static void main(String args[])
     {
         // Call BaseTests Class
         BaseTests test= new BaseTests();
         try {
-            test.setUp();
+            //test.setUp();
+            test.shiftContent();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
 
     }
 
